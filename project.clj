a613145bf3b0d481475a3ab2b7361399482641ea
Martino Visintin <martino.visintin@ovoenergy.com>
2019-01-11 16:48:20 +0000

    upgrade to kafka v2

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject ovotech/kafka-clj-test-utils "1.1.1-1"
+(defproject ovotech/kafka-clj-test-utils "2.0.1-1"
   :description "Companion test utility library for `ovotech/kafka-clj-utils`"
   :url "https://github.com/ovotech/kafka-clj-test-utils"
   :license {:name "Eclipse Public License"
             :url  "https://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.apache.kafka/kafka-clients "1.1.1" :exclusions [org.scala-lang/scala-library]]
+  :dependencies [[org.apache.kafka/kafka-clients "2.0.1" :exclusions [org.scala-lang/scala-library]]
                  [org.clojure/clojure "1.9.0"]
-                 [ovotech/kafka-avro-confluent "0.11.0"]
-                 [vise890/zookareg "1.1.1-1"]]
+                 [ovotech/kafka-avro-confluent "2.0.1-1"]
+                 [vise890/zookareg "2.0.1-1"]]
 
   :profiles {:dev {:resource-paths ["dev/resources" "test/resources"]
                    :dependencies [[ch.qos.logback/logback-classic "1.2.3"]
