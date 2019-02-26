00330ae5a9acf42fde3a4e8ead5dbfd4a7f6080a
Gavin Esberger <gavin.esberger@ovoenergy.com>
2019-02-26 12:02:05 +0000

    Upgrading to Kafka 2.1.0

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject ovotech/kafka-clj-test-utils "2.0.1-3"
+(defproject ovotech/kafka-clj-test-utils "2.1.0-1"
   :description "Companion test utility library for `ovotech/kafka-clj-utils`"
   :url "https://github.com/ovotech/kafka-clj-test-utils"
   :license {:name "Eclipse Public License"
             :url  "https://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.apache.kafka/kafka-clients "2.0.1" :exclusions [org.scala-lang/scala-library]]
+  :dependencies [[org.apache.kafka/kafka-clients "2.1.0" :exclusions [org.scala-lang/scala-library]]
                  [org.clojure/clojure "1.9.0"]
-                 [ovotech/kafka-avro-confluent "2.0.1-1"]
-                 [vise890/zookareg "2.0.1-1"]]
+                 [ovotech/kafka-avro-confluent "2.1.0-1"]
+                 [vise890/zookareg "2.1.0-1"]]
 
   :profiles {:dev {:resource-paths ["dev/resources" "test/resources"]
                    :dependencies [[ch.qos.logback/logback-classic "1.2.3"]
