7a3184d4cb2b57642584d935c773c452e7552cd2
oli <oli.desborough@ovoenergy.com>
2020-07-09 16:21:56 +0100

    Upgraded kac and zookareg

--- project.clj
+++ project.clj
@@ -4,12 +4,11 @@
   :license {:name "Eclipse Public License"
             :url  "https://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.apache.kafka/kafka-clients "2.4.1" :exclusions [org.scala-lang/scala-library]]
-                 [org.clojure/clojure "1.9.0"]
-                 [ovotech/kafka-avro-confluent "2.1.0-2"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [ovotech/kafka-avro-confluent "2.4.1-1"]]
 
   :profiles {:dev {:resource-paths ["dev/resources" "test/resources"]
-                   :dependencies [[vise890/zookareg "2.1.0-1"]
+                   :dependencies [[vise890/zookareg "2.4.1-1"]
                                   [ch.qos.logback/logback-classic "1.2.3"]
                                   [ch.qos.logback/logback-core "1.2.3"]]}
              :ci  {:deploy-repositories
